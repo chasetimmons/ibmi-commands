b6aeb6887fe7e2c35bd1f25667f9ed88f0dabd0d
chasetimmons <38573434+chasetimmons@users.noreply.github.com>
2024-01-12 15:49:37 -0800

    Create qry.cmd

---
+++ qry.cmd
@@ -0,0 +1,4 @@
+             CMD        PROMPT('QUERY A FILE')                        
+                                                                      
+             PARM       KWD(LIB) TYPE(*CHAR) LEN(10) PROMPT('LIBRARY NAME')                                      
+             PARM       KWD(FIL) TYPE(*CHAR) LEN(10) PROMPT('FILE NAME')                                      
