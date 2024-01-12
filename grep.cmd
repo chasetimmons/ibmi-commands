571011d45952ba7f949eb81c61972fa09a125d9b
chasetimmons <38573434+chasetimmons@users.noreply.github.com>
2024-01-12 15:04:18 -0800

    Create grep.cmd
    Added QCMDSRC.LIB/GREP.MBR

---
+++ grep.cmd
@@ -0,0 +1,5 @@
+             CMD        PROMPT('SEARCH SOURCE MEMBER FOR TEXT')      
+                                                                     
+             PARM       KWD(TXT) TYPE(*CHAR) LEN(20) PROMPT('SEARCH STRING')                                   
+             PARM       KWD(LIB) TYPE(*CHAR) LEN(6) PROMPT('LIBRARY')
+             PARM       KWD(FIL) TYPE(*CHAR) LEN(10) PROMPT('FILE')  
