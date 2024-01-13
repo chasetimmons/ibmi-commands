e46b04a1fd616c07609a45ff37678684f25049e4
chasetimmons <38573434+chasetimmons@users.noreply.github.com>
2024-01-12 16:46:10 -0800

    Update qry.cmd
    Fixed parameter order

--- qry.cmd
+++ qry.cmd
@@ -1,4 +1,4 @@
-             CMD        PROMPT('QUERY A FILE')                        
-                                                                      
-             PARM       KWD(LIB) TYPE(*CHAR) LEN(10) PROMPT('LIBRARY NAME')                                      
-             PARM       KWD(FIL) TYPE(*CHAR) LEN(10) PROMPT('FILE NAME')                                      
+             CMD        PROMPT('QUERY A FILE')
+
+             PARM       KWD(FIL) TYPE(*CHAR) LEN(10) PROMPT('FILE NAME')
+             PARM       KWD(LIB) TYPE(*CHAR) LEN(10) PROMPT('LIBRARY NAME')
