469a1dfc78941ed1676b08a4d36071c8b79d9796
chasetimmons <38573434+chasetimmons@users.noreply.github.com>
2024-01-15 16:39:06 -0800

    Delete grep.cmd

--- grep.cmd
+++
@@ -1,5 +0,0 @@
-             CMD        PROMPT('SEARCH SOURCE MEMBER FOR TEXT')      
-                                                                     
-             PARM       KWD(TXT) TYPE(*CHAR) LEN(20) PROMPT('SEARCH STRING')                                   
-             PARM       KWD(LIB) TYPE(*CHAR) LEN(10) DFT('*') PROMPT('LIBRARY')
-             PARM       KWD(FIL) TYPE(*CHAR) LEN(10) DFT('*') PROMPT('FILE')  
