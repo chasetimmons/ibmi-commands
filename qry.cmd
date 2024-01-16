c715a8571802e83fc2a3d32f385d96999ea70836
chasetimmons <38573434+chasetimmons@users.noreply.github.com>
2024-01-15 16:38:58 -0800

    Delete qry.cmd

--- qry.cmd
+++
@@ -1,4 +0,0 @@
-             CMD        PROMPT('QUERY A FILE')
-
-             PARM       KWD(FIL) TYPE(*CHAR) LEN(10) PROMPT('FILE NAME')
-             PARM       KWD(LIB) TYPE(*CHAR) LEN(10) PROMPT('LIBRARY NAME')
