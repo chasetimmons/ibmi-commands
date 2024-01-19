6572939523b749662a6cb1dd7d35f8f81ac1d240
chasetimmons <38573434+chasetimmons@users.noreply.github.com>
2024-01-19 10:05:23 -0800

    Added output flag "O"

--- qcmdsrc/grep.cmd
+++ qcmdsrc/grep.cmd
@@ -3,4 +3,4 @@
              PARM       KWD(TXT) TYPE(*CHAR) LEN(20) PROMPT('SEARCH STRING')               
              PARM       KWD(LIB) TYPE(*CHAR) LEN(10) DFT('*') PROMPT('LIBRARY')                     
              PARM       KWD(FIL) TYPE(*CHAR) LEN(10) DFT('*') PROMPT('FILE')
-             PARM       KWD(FLG) TYPE(*CHAR) LEN(1) RSTD(*YES) VALUES(T) PROMPT('TEST FLAG')
+             PARM       KWD(FLG) TYPE(*CHAR) LEN(1) RSTD(*YES) VALUES(T O) PROMPT('FLAG')
