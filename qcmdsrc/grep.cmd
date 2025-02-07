16063a0ad84f019e0cd6953dcf743c14a1458ab7
chasetimmons <38573434+chasetimmons@users.noreply.github.com>
2025-02-07 09:41:57 -0800

    Update grep.cmd

--- qcmdsrc/grep.cmd
+++ qcmdsrc/grep.cmd
@@ -1,6 +1,5 @@
-             CMD        PROMPT('SEARCH SOURCE MEMBER FOR TEXT') 
-                                                                
-             PARM       KWD(TXT) TYPE(*CHAR) LEN(20) PROMPT('SEARCH STRING')               
-             PARM       KWD(LIB) TYPE(*CHAR) LEN(10) DFT('*') PROMPT('LIBRARY')                     
+             CMD        PROMPT('SEARCH SOURCE MEMBER FOR TEXT')
+             PARM       KWD(TXT) TYPE(*CHAR) LEN(20) PROMPT('SEARCH STRING')
+             PARM       KWD(LIB) TYPE(*CHAR) LEN(10) DFT('*') PROMPT('LIBRARY')
              PARM       KWD(FIL) TYPE(*CHAR) LEN(10) DFT('*') PROMPT('FILE')
              PARM       KWD(FLG) TYPE(*CHAR) LEN(1) RSTD(*YES) VALUES(T O) PROMPT('FLAG')
