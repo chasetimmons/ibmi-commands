be293f64a364cc7be7bc81af1ee517586d2b07f8
chasetimmons <38573434+chasetimmons@users.noreply.github.com>
2025-02-11 12:51:25 -0800

    Update count.cmd

--- qcmdsrc/count.cmd
+++ qcmdsrc/count.cmd
@@ -1,3 +1,2 @@
              CMD        PROMPT('COUNT MEMBERS IN A LIBRARY')                                        
-             PARM       KWD(LIB) TYPE(*CHAR) LEN(10) DFT('*') +
-                          PROMPT('LIBRARY')                    
+             PARM       KWD(LIB) TYPE(*CHAR) LEN(10) DFT('*') PROMPT('LIBRARY')
